cff54dc2f78dfd136ce5d885137133c122d64888
Solomon <ndifereke.solomon@duronetworks.com>
2020-10-25 20:47:09 +0100

    some changes to techinnover

--- app.js
+++ app.js
@@ -44,12 +44,8 @@ con.on("open", () => {
   console.log("connected...");
 });
 
-app.get("/start", (req, res) => {
-  res.json({ message: "Welcome sir" });
-});
-
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/client/public/index.html"));
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
 });
 
 const server = http.listen(PORT, "localhost", () => {
